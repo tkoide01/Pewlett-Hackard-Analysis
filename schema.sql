c3dca9e21f7e2fa62a9d15ba177061a992ec8e3f
Takuma Koide <tkoide1210@gmail.com>
2021-04-09 18:38:19 -0400

    add

--- schema.sql
+++ schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(5) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -35,3 +35,27 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE dept_employees (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(5) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no,dept_no)
+);
+
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR(20) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, to_date)	
+);
+
+DROP TABLE titles CASCADE;
+DROP TABLE dept_employees CASCADE;
+
+SELECT * FROM titles
